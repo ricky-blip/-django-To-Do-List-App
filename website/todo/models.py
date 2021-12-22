82bb255c900e57825562b31c2ca9e6322964494a
ricky-blip <ricky.rinaldy77@gmail.com>
2021-12-23 06:18:23 +0700

    update models

--- website/todo/models.py
+++ website/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.TextField(max_length=50)
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return '%s: %s' % (self.id, self.title)
+        
+        
--- website/todo/views.py
+++ website/todo/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
+
 # Create your views here.
 
 # function index
 def index(request):
+    items = Todo.objects.order_by('-id')
     # return HttpResponse("Hello Django")
-    return render(request, 'todo/index.html')
+    return render(request, 'todo/index.html', {'items': items})
 
 # function button Done
 def done(request):
-    return render(request, 'todo/index.html')
+    items = Todo.objects.filter(status=True).order_by('-id')
+    return render(request, 'todo/index.html', {'items': items})
 
 # function button Pending
 def pending(request):
-    return render(request, 'todo/index.html')
+    items = Todo.objects.filter(status=False).order_by('-id')
+    return render(request, 'todo/index.html', {'items': items})
 
 # function button Delete All
 def delete_all(request):
